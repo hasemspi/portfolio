59a1f34385e12f9df9d69352c32f2decda8b46d7
Hasem <you@example.com>
2023-03-09 23:56:36 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import ParticlesBackground from './Components/ParticlesBackground';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+   <div className="App">
+     <ParticlesBackground/>
+      <h1 style={{backgroundColor:"#fff",padding:'50px'}}>We are Ready Portfolio Website Developement</h1>
+
+      
     </div>
+    
+ 
   );
 }
 
