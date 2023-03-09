9922dc2fb9bbc80b8e01e30cb39ad1b72459f358
Hasem <you@example.com>
2023-03-10 00:24:38 +0600

    update data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
    
    <div className="App">
      <ParticlesBackground/>
-      <h1 style={{backgroundColor:"#fff",padding:'50px'}}>We are Ready Portfolio Website Developement</h1>
+     <button className="btn">Button</button>
+    <button className="btn btn-primary">Button</button>
+    <button className="btn btn-secondary">Button</button>
+    <button className="btn btn-accent">Button</button>
+    <button className="btn btn-ghost">Button</button>
+    <button className="btn btn-link">Button</button>
 
       
     </div>
